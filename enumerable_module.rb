d3e6c7e2ee25933753e94fb6fb922b5b27cbe5fa
tiagorahal <rahal.aires@gmail.com>
2021-10-15 15:30:41 -0300

    Fix linter issues

--- enumerable_module.rb
+++ enumerable_module.rb
@@ -1,25 +1,25 @@
 module MyEnumerable
-    def all?
-      all = true
-      list.each do |i|
-        all = false unless yield(i)
-      end
-      all
+  def all?
+    all = true
+    list.each do |i|
+      all = false unless yield(i)
     end
-  
-    def any?
-      any = false
-      list.each do |i|
-        any = true if yield(i)
-      end
-      any
-    end
-  
-    def filter
-      filter = []
-      list.each do |i|
-        filter.push(i) if yield(i)
-      end
-      filter
-    end
+    all
   end
+
+  def any?
+    any = false
+    list.each do |i|
+      any = true if yield(i)
+    end
+    any
+  end
+
+  def filter
+    filter = []
+    list.each do |i|
+      filter.push(i) if yield(i)
+    end
+    filter
+  end
+end
